fd86ee4337c0be0e6d4ce84510ac0ab36851ff22
Kenneth Ashley <ken.u.diggitt@gmail.com>
2025-01-14 15:22:55 -0500

    fix generation responses

--- agent/src/lib/templates.ts
+++ agent/src/lib/templates.ts
@@ -1,4 +1,4 @@
 export const TEMPLATES = {
-    generationSuccess: `create an informal response to you showing a cool video that you created. add some allure. add "$apple" at the end. make it extremely short. dont use hashtags.`,
-    generationError: `create a formal but chill message to a video that failed to get generated. don't mention '$apple'. make it extremely short`,
+    generationSuccess: `create an informal response to you showing a cool video that you created. use words like 'this video'. add some allure. add "$apple" at the end. make it extremely short. dont use hashtags.`,
+    generationError: `create a formal but chill message to a video that failed to get generated. don't mention '$apple'. make it extremely short and say you've let the team know`,
 };
